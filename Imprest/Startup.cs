435749ade27529c7076dfdab4c074bd4e73b6b71
PrabaHome <prababoy123@gmail.com>
2017-06-19 21:35:33 +0530

    Add project files.

---
+++ Imprest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Imprest.Startup))]
+namespace Imprest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
